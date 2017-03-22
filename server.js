7161dd353c7e0fbf4fe00fe9c14aee48e543bd73
Roody <rudi.mottinger@gmail.com>
2017-03-22 13:53:41 +0100

    revertetet test for html

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var http = require('http');
-fs = require('fs');
 
-fs.readFile('public/index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8000);
-});
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+
+}).listen(process.env.PORT || 8080);

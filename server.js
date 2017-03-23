643bafd0caa64ea4e7adab289c58fce77d7eef55
Roody <rudi.mottinger@gmail.com>
2017-03-23 09:51:01 +0100

    edited for sending

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const collection = 'combewertung';
 var MongoClient = require("mongodb").MongoClient;
 var mongodb;
 
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream('../debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+    log_file.write(util.format(d) + '\n');
+    log_stdout.write(util.format(d) + '\n');
+};
+
 app.get('/', function(req, res) {
     res.redirect('/public/index.html');
 });

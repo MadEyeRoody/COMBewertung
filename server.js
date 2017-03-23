ca88402103cb753ca7ed3180c9341b4fbfb5edf6
Roody <rudi.mottinger@gmail.com>
2017-03-23 10:30:21 +0100

    edited for sending

--- server.js
+++ server.js
@@ -5,20 +5,6 @@ const collection = 'combewertung';
 var MongoClient = require("mongodb").MongoClient;
 var mongodb;
 
-var fs = require('fs');
-var util = require('util');
-var log_file = fs.createWriteStream('logs/debug.log', {flags : 'w'});
-var log_stdout = process.stdout;
-
-console.log = function(d) { //
-    log_file.write(util.format(d) + '\n');
-    log_stdout.write(util.format(d) + '\n');
-};
-
-app.get('/', function(req, res) {
-    res.redirect('/public/index.html');
-});
-
 
 app.get('/speichereBewertung', function (req, res) {
     MongoClient.connect("mongodb://combewertung:3dKmkmTw6kh2KIkbpZ2R6aYVpVYSatM2fjK41V0fSxrsAlzS1mvKW9tWn5nqi2r3Kp34Qnm1ebxnaJQem3ximQ==@combewertung.documents.azure.com:10250/?ssl=true",

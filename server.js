89d300fec5636e0ae94d6c700c7c9bac5856db99
Roody <rudi.mottinger@gmail.com>
2017-03-23 10:34:04 +0100

    edited for sending

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var MongoClient = require("mongodb").MongoClient;
 var mongodb;
 
 
+app.get('/', function(req, res) {
+    res.redirect('/public/index.html');
+});
+
+
 app.get('/speichereBewertung', function (req, res) {
     MongoClient.connect("mongodb://combewertung:3dKmkmTw6kh2KIkbpZ2R6aYVpVYSatM2fjK41V0fSxrsAlzS1mvKW9tWn5nqi2r3Kp34Qnm1ebxnaJQem3ximQ==@combewertung.documents.azure.com:10250/?ssl=true",
         function(err, db) {

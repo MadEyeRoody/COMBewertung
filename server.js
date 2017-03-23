080d086fae7da18ed6c9fdaabc0b5c546a4c1bb3
Roody <rudi.mottinger@gmail.com>
2017-03-23 11:50:36 +0100

    edited for sending

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 var express = require('express');
-var http = require('http');
 var app = express();
 const collection = 'combewertung';
 
 var MongoClient = require("mongodb").MongoClient;
 var mongodb;
 
-http.createServer(function (req, res) {
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream('logs/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+    log_file.write(util.format(d) + '\n');
+    log_stdout.write(util.format(d) + '\n');
+};
+
+
+app.get('/', function(req, res) {
     res.redirect('index.html');
-}).listen(process.env.PORT || 8080);
+});
 
 
-app.get('speichereBewertung', function (req, res) {
+app.get('/speichereBewertung', function (req, res) {
     MongoClient.connect("mongodb://combewertung:3dKmkmTw6kh2KIkbpZ2R6aYVpVYSatM2fjK41V0fSxrsAlzS1mvKW9tWn5nqi2r3Kp34Qnm1ebxnaJQem3ximQ==@combewertung.documents.azure.com:10250/?ssl=true",
         function(err, db) {
             if (err) {
@@ -31,4 +41,3 @@ app.get('speichereBewertung', function (req, res) {
 
 })
 app.listen(process.env.PORT || 8080);
-

1892e070af0f30af2c06126886429a823decd4b3
Roody <rudi.mottinger@gmail.com>
2017-03-24 13:56:41 +0100

    edited for sending

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/api/getBewertung', function (req, res) {
                         console.log(docs[0].stationShort)
                         var id = 0;
                         for (var doc in docs){
-                            console.log(doc)
+                            doc = doc.parseJSON
                             if(doc.stationShort== req.query.id){
                                 resDocs.push(doc[id]);
                             }
